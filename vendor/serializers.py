370eb09de5cce30a77b01e1fc7a19ab72a8defff
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-05-13 11:22:46 +0530

    stats

--- vendor/serializers.py
+++ vendor/serializers.py
@@ -4,19 +4,20 @@ from account.models import User
 from client.models import Order
 from client.serializer import MidOrderSerializer
 
+class UserVSerializer(MidOrderSerializer):
+    class Meta:
+        model = User
+        fields = ['name', 'ph_number']
 
 class OrderVSerializer(ModelSerializer):
     midorder_set = MidOrderSerializer(many=True)
+    user = UserVSerializer()
     class Meta:
         model = Order
         fields = ['oid', 'address', 'date_time',
-                  'payment_method', 'midorder_set', 'total_amount', 'status']
+                  'payment_method', 'midorder_set', 'total_amount', 'status', 'user']
 
 
 # class MidOrderVSerializer(MidOrderSerializer):
 #     order = OrderVSerializer()
 
-class UserVSerializer(MidOrderSerializer):
-    class Meta:
-        model = User
-        fields = ['name', 'ph_number']
